df205b5c03b8716bd6255bf7124a4924bb739e1d
LAPTOP-HFCEP3M4\accou <accounts000@protonmail.com>
2021-01-26 16:38:43 -0800

    Builds and deploys. Added main XAML view.

--- SoundboardTemplate.Android/obj/Debug/100/android/environment.armeabi-v7a.s
+++ SoundboardTemplate.Android/obj/Debug/100/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"4179856b-3c87-47c4-9741-d5387ba35a0e"
+	.asciz	"bda55711-e82c-40a0-9a52-e8313928b6e7"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
